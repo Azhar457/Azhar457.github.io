bb9be4996bf680ce1138b47cd561b8df531c38e8
Azharmtq <azhar.233040126@mail.unpas.ac.id>
2024-06-03 10:09:42 +0700

    hero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
-    if (window.scrollY > 40) { // Nilai 50 dapat disesuaikan
+    if (window.scrollY > 10) { // Nilai 50 dapat disesuaikan
         navbar.classList.add('fixed-top');
     } else {
         navbar.classList.remove('fixed-top');
